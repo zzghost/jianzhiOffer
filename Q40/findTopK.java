208384f47872f5c03298099d1e80b07a0f46f3ed
zzghost <zhouliu712@gmail.com>
2017-05-31 23:15:28 +0800

    最小的k个数

--- Q40/findTopK.java
+++ Q40/findTopK.java
@@ -6,7 +6,13 @@
 //解法1：直接调用java的排序函数，取前k个o(nlogn)，时间37ms
 //解法2：快速排序的分治思想。取得的k个数未必是排好序的。时间39ms
 //解法3：使用java里的treeset，treeset底层是红黑树实现的。treeset保存最小的k个数。时间38ms
-public class Solution {
+//
+//
+//
+//第三种解法适合处理海量数据
+//第二种解法会在原数组上做改动
+//可以问清楚需求，再采用相应的方法做
+public class Solution1 {
     public ArrayList<Integer> GetLeastNumbers_Solution(int [] input, int k) {
         java.util.ArrayList<Integer> aList = new java.util.ArrayList<Integer>();
         if(k == 0 || input == null || input.length == 0 || input.length < k) return aList;
@@ -16,8 +22,43 @@ public class Solution {
         return aList;
     }
 }
-
-public class Solution {
+import java.util.ArrayList;
+ 
+public class Solution2 {
+        public int partition(int[] input, int start, int end){
+                    int first = input[start];
+                            while(start < end){
+                                            while(input[end] > first && end > start) end--;
+                                                        if(end > start)
+                                                                            input[start++] = input[end];
+                                                                    while(input[start] < first && end > start) start++;
+                                                                                if(end > start)
+                                                                                                    input[end--] = input[start];
+                                                                                        }
+                                    input[start] = first;
+                                            return start;
+                                                }
+            public ArrayList<Integer> GetLeastNumbers_Solution(int [] input, int k) {
+                        java.util.ArrayList<Integer> aList = new java.util.ArrayList<Integer>();
+                                if(k == 0 || input == null || input.length == 0 || input.length < k) return aList;
+                                        int start = 0, end = input.length - 1;
+                                                int idx = partition(input, start, end);
+                                                        while(idx != k - 1){
+                                                                        if(idx > k - 1){
+                                                                                            end = idx - 1;
+                                                                                                            idx = partition(input, start, end);
+                                                                                                                        }
+                                                                                    else{
+                                                                                                        start = idx + 1;
+                                                                                                                        idx = partition(input, start, end);
+                                                                                                                                    }
+                                                                                            }
+                                                                for(int i = 0; i < k; i++)
+                                                                                aList.add(input[i]);
+                                                                        return aList;
+                                                                            }
+}
+public class Solution3 {
     public int partition(int[] input, int start, int end){
         int first = input[start];
         while(start < end){
